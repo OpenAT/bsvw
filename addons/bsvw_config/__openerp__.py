8b9cc0d34d16956632181d87aa3722fbe83dd1d6
Joachim Grubelnik <j.grubelnik@openat.at>
2020-09-15 15:36:40 +0200

    [ADD] rebuild the old customer addon

---
+++ addons/bsvw_config/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'bsvw_config',
+    'summary': '''FS-Online bsvw instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance bsvw
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Joachim Grubelnik (joachim.grubelnik@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'sale',
+        'portal_sale',
+        'website',
+        'website_sale_donate',
+    ],
+    'data': [
+        'views/templates.xml',
+        'views/snippet_options.xml',
+    ],
+}
